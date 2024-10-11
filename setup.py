d27b4c84c6db3ef5c29eb81533777c85ee41e177
Erica Camila <ericamila0002@gmail.com>
2024-10-10 20:04:59 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="teste_ericamila0002",
     version="0.0.1",
     author="my_name",
     author_email="my_email",
     description="My short description",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
